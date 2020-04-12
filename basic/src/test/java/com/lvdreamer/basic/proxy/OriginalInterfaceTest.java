0ec439983b51681dab586c0706a6a94c473aa6d9
lvdreamer <36273229@qq.com>
2020-04-12 22:38:14 +0800

    add proxy module

---
+++ basic/src/test/java/com/lvdreamer/basic/proxy/OriginalInterfaceTest.java
@@ -0,0 +1,29 @@
+package com.lvdreamer.basic.proxy;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import static org.junit.Assert.*;
+
+public class OriginalInterfaceTest {
+
+    @Test
+    public void sayHello() {
+        OriginalInterface originalInterface = new OriginalObject();
+        OriginalProxy originalProxy = new OriginalProxy(originalInterface);
+        OriginalInterface proxyInstance = (OriginalInterface) Proxy.newProxyInstance(originalProxy.getClass().getClassLoader(), originalInterface.getClass().getInterfaces(), originalProxy);
+        proxyInstance.sayHello();
+        System.out.println("=======================================================================");
+        InvocationHandler invocationHandler = (proxy, method, args) -> {
+            System.out.println("anonymous proxy before");
+            method.invoke(originalInterface, args);
+            System.out.println("anonymous proxy after");
+            return null;
+        };
+        OriginalInterface proxyInstance2 = (OriginalInterface) Proxy.newProxyInstance(originalProxy.getClass().getClassLoader(), originalInterface.getClass().getInterfaces(), invocationHandler);
+        proxyInstance2.sayHello();
+    }
+}
