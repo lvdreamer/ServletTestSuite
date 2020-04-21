111765d6c9f2c1fc4fa94c537976453c76bbcba8
lvdreamer <36273229@qq.com>
2020-04-21 20:49:58 +0800

    add comment

--- algorithm/src/main/java/com/lvdreamer/algorithm/MinimumFactorization.java
+++ algorithm/src/main/java/com/lvdreamer/algorithm/MinimumFactorization.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * leetcode No.625
  * 给定一个正整数a，找到最小的正整数b，它的每个数字相乘之后等于a。
  * <p>
  * 如果没有答案，或者答案超过了32位有符号整型的范围，返回0。
@@ -19,24 +20,26 @@ public class MinimumFactorization {
     }
 
     public static int smallestFactorization2(int n) {
-        // Case 1: If number is smaller than 10
         if (n < 10) return n;
 
-        // Case 2: Start with 9 and try every possible digit
         List<Integer> res = new ArrayList<>();
         for (int i = 9; i > 1; i--) {
-            // If current digit divides n, then store all
-            // occurrences of current digit in res
+
+            /**
+             * 如果可以被i整除，对除数继续循环
+             */
             while (n % i == 0) {
                 n = n / i;
                 res.add(i);
             }
         }
 
-        // If n could not be broken in form of digits
+        /**
+         * 没除尽，表示没有最小因子
+         */
         if (n != 1) return 0;
 
-        // Get the result from the array in reverse order
+        //最小数子
         long result = 0;
         for (int i = res.size() - 1; i >= 0; i--) {
             result = result * 10 + res.get(i);
