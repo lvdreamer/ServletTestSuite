f1c8df9681f2a2694187076a3dff51700ae95269
lvdreamer <36273229@qq.com>
2020-05-17 22:52:52 +0800

    leet code solution question5

--- algorithm/src/test/java/com/lvdreamer/algorithm/Question0625Test.java
+++ algorithm/src/test/java/com/lvdreamer/algorithm/Question0625Test.java
@@ -111,50 +111,6 @@ public class Question0625Test {
         }
     }
 
-    /**
-     * 最长回文子串
-     * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000
-     */
-    @Test
-    public void solution3() {
-        System.out.println(solution3("babad"));
-
-    }
-
-    private String solution3(String sourceStr) {
-        if (null == sourceStr || sourceStr.length() == 0) {
-            return "";
-        }
-        int strLegth = sourceStr.length();
-        int maxStart = 0;
-        int maxLen = 0;
-        for (int i = 0; i < strLegth; i++) {
-            int left = i - 1;
-            int rigth = i + 1;
-            int curLen = 1;
-            char middle = sourceStr.charAt(i);
-            while (left >= 0 && sourceStr.charAt(left) == middle) {
-                left--;
-                curLen++;
-            }
-            while (rigth < strLegth && sourceStr.charAt(rigth) == middle) {
-                rigth++;
-                curLen++;
-            }
-            while (left >= 0 && rigth < strLegth && sourceStr.charAt(left) == sourceStr.charAt(rigth)) {
-                left--;
-                rigth++;
-                curLen += 2;
-            }
-            if (curLen > maxLen) {
-                maxLen = curLen;
-                maxStart = left;
-            }
-        }
-        return sourceStr.substring(maxStart + 1, maxStart + maxLen + 1);
-
-
-    }
 
     @Test
     public void mySqrtTest() {
