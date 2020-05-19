a9e109d0e2f199281a8e9a5e0637f1dfafe022be
lvdreamer <36273229@qq.com>
2020-05-19 23:25:50 +0800

    leet code solution question6

---
+++ algorithm/src/main/java/com/lvdreamer/algorithm/Question0006.java
@@ -0,0 +1,21 @@
+package com.lvdreamer.algorithm;
+
+/**
+ * 6. Z 字形变换
+ * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zigzag-conversion/
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Question0006 {
+    public static void main(String[] args) {
+        String res = new Question0006().convert("babad", 3);
+        System.out.println(res);
+
+    }
+
+    public String convert(String s, int numRows) {
+        return "";
+    }
+}
