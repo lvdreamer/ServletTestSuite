6ea1a90d14abc4aa9c1ed5fb4216a2e4899341f7
lvdreamer <36273229@qq.com>
2020-06-27 23:27:20 +0800

    springbatch init

---
+++ spring-batch/src/main/java/com/lvdreamer/springbatch/SpringBatchApplication.java
@@ -0,0 +1,11 @@
+package com.lvdreamer.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBatchApplication.class, args);
+    }
+}
