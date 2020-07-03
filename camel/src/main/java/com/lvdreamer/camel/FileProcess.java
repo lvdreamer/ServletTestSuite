090017e4ad181f484d85025fd0c80eced51539f4
lvdreamer <36273229@qq.com>
2020-07-03 22:34:28 +0800

    camel demo

--- camel/src/main/java/com/lvdreamer/camel/FileProcess.java
+++ camel/src/main/java/com/lvdreamer/camel/FileProcess.java
@@ -2,11 +2,13 @@ package com.lvdreamer.camel;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.camel.component.file.GenericFileMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.RandomAccessFile;
 
 @Component
 public class FileProcess implements Processor {
@@ -14,11 +16,13 @@ public class FileProcess implements Processor {
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        logger.info("文件名 ： " + exchange.getIn().getHeader(Exchange.FILE_NAME));
-        logger.info("绝对路径 ： " + exchange.getIn().getHeader("CamelFileAbsolutePath"));
-        logger.info("最后修改时间 ： " + exchange.getIn().getHeader("CamelFileLastModified"));
-        logger.info("文件大小： " + exchange.getIn().getHeader("CamelFileLength"));
-        Object body = exchange.getIn().getBody();
+        GenericFileMessage<File> inFileMessage = (GenericFileMessage<File>) exchange.getIn();
+
+        logger.info("文件名 ： " + inFileMessage.getHeader(Exchange.FILE_NAME));
+        logger.info("绝对路径 ： " + inFileMessage.getHeader("CamelFileAbsolutePath"));
+        logger.info("最后修改时间 ： " + inFileMessage.getHeader("CamelFileLastModified"));
+        logger.info("文件大小： " + inFileMessage.getHeader("CamelFileLength"));
+        File body = inFileMessage.getGenericFile().getFile();
         logger.info(String.valueOf(body instanceof File));
         if (exchange.getIn().getHeader(Exchange.FILE_NAME).equals("multi_1_2b47fef37d75408ba5292da8f3f3bc3e-宽表测试数据.txt")) {
             throw new RuntimeException("异常文件");
--- camel/src/main/java/com/lvdreamer/camel/FileRouter.java
+++ camel/src/main/java/com/lvdreamer/camel/FileRouter.java
@@ -11,19 +11,17 @@ import org.springframework.stereotype.Component;
 public class FileRouter extends RouteBuilder {
     private static Logger logger = LoggerFactory.getLogger(FileRouter.class);
 
-    private String localFileDir = "file:/data/dna/handfile?delay=1s&move=../backup/${date:now:yyyyMMdd}/${file:name}";
+    private String localFileDir = "file:/data/dna/handfile/?delay=5s&move=backup/${date:now:yyyyMMdd}/${file:name}&moveFailed=error/${date:now:yyyyMMdd}/${file:name}";
     @Autowired
     private FileProcess fileProcess;
 
     @Override
     public void configure() throws Exception {
-        onException(Exception.class)
-                .handled(true)// 路由停止将错误信息返回给最初的消费者
-                .log(LoggingLevel.ERROR, logger, "Exception occurred due: ${exception.message}")
-                .to("file:/data/dna/error").end();
+
         from(localFileDir)    //数据源
-                .log(LoggingLevel.INFO, logger, "Downloaded file ${file:name} complete.")
-                .process(fileProcess); //处理器
+                .log(LoggingLevel.INFO, logger, "process file ${file:name} start.")
+                .process(fileProcess)//处理器
+                .log(LoggingLevel.INFO, logger, "process file ${file:name} end.");
 
     }
 }
