4b2a30c58a06919367c48ff737b826d365e1f604
lvdreamer <36273229@qq.com>
2020-11-29 20:54:52 +0800

    json test

---
+++ basic/src/test/java/com/lvdreamer/basic/JsonTest.java
@@ -0,0 +1,16 @@
+package com.lvdreamer.basic;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.junit.Test;
+
+public class JsonTest {
+
+    @Test
+    public void ex(){
+        String response="{}";
+        Math respResult = new Gson().fromJson(response, new TypeToken<Math>() {
+        }.getType());
+        System.out.println(respResult);
+    }
+}
