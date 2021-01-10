738c038a3d106f4cec6f35cd71103a1d1ac3aaf0
lvdreamer <36273229@qq.com>
2021-01-10 23:11:38 +0800

    json序列化

--- redis/src/main/java/com/lvdreamer/redis/RedisConfiguration.java
+++ redis/src/main/java/com/lvdreamer/redis/RedisConfiguration.java
@@ -1,8 +1,14 @@
 package com.lvdreamer.redis;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.xml.internal.ws.encoding.soap.SerializationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
@@ -11,11 +17,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.nio.charset.Charset;
 
 @Configuration
 @AutoConfigureAfter(RedisAutoConfiguration.class)
 public class RedisConfiguration {
+    private static final Logger logger = LoggerFactory.getLogger(RedisConfiguration.class);
+
+    private String namespace = "lvdreamer";
 
     @Bean("redisTemplate")
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
@@ -30,5 +42,73 @@ public class RedisConfiguration {
         template.setKeySerializer(new StringRedisSerializer());
         template.afterPropertiesSet();
         return template;
+    }
+
+    @Bean("redisTemplate2")
+    public RedisTemplate<String, Object> redisTemplate2(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+        FastJsonRedisSerializer jsonSerializer = new FastJsonRedisSerializer<>(Object.class);
+        // 设置值（value）的序列化采用FastJsonRedisSerializer。
+        redisTemplate.setValueSerializer(jsonSerializer);
+        // 设置键（key）的序列化采用StringRedisSerializer。
+        redisTemplate.setKeySerializer(keyStringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    @Bean
+    public StringRedisSerializer keyStringRedisSerializer() {
+
+        return new StringRedisSerializer() {
+            @Override
+            public String deserialize(byte[] bytes) {
+                String originalKey = super.deserialize(bytes);
+                int indexOf = originalKey.indexOf(namespace);
+                if (indexOf > 0) {
+                    logger.info("key缺少前缀");
+                } else {
+                    originalKey = originalKey.substring(indexOf);
+                }
+                return originalKey;
+            }
+
+            @Override
+            public byte[] serialize(String string) {
+                return super.serialize(namespace + string);
+            }
+        };
+    }
+}
+
+class FastJsonRedisSerializer<T> implements RedisSerializer<T> {
+
+    public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+
+    private Class<T> clazz;
+
+    public FastJsonRedisSerializer(Class<T> clazz) {
+        super();
+        this.clazz = clazz;
+    }
+
+    @Override
+    public byte[] serialize(T t) throws SerializationException {
+        if (t == null) {
+            return new byte[0];
+        }
+        return JSON.toJSONString(t, SerializerFeature.WriteClassName).getBytes(DEFAULT_CHARSET);
+    }
+
+    @Override
+    public T deserialize(byte[] bytes) throws SerializationException {
+        if (bytes == null || bytes.length <= 0) {
+            return null;
+        }
+        String str = new String(bytes, DEFAULT_CHARSET);
+        return JSON.parseObject(str, clazz);
     }
 }
